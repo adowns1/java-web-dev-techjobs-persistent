3e820302f7f74c99d80afa17b31a191e061cf82e
ashleydowns <netzhammer15@gmail.com>
2020-02-06 18:28:07 -0600

    first commit, attach mySQL

--- queries.sql
+++ queries.sql
@@ -1,5 +1,9 @@
 ## Part 1: Test it with SQL
-
+   columns
+    id INTEGER
+    employer VARCHAR(20)
+    name VARCHAR(20)
+    skills VARCHAR(200)
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
