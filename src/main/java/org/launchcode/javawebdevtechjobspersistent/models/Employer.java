1670586b258f2c68a978e687c6cf1b4091353c75
ashleydowns <netzhammer15@gmail.com>
2020-02-06 19:24:25 -0600

    added repositories

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,22 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+
+@Entity
 public class Employer extends AbstractEntity {
+    @Size(min = 3, max = 100, message = "Enter Location")
+    public String location;
 
+    public Employer() {
+    }
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
-public class Skill extends AbstractEntity {
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
 
+@Entity
+public class Skill extends AbstractEntity {
+    @Size(min = 3, message = "add description")
+    public String description;
+
+    public Skill(){
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
