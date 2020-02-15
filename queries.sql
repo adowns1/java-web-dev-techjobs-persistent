dd655200218c974356ba630a7b3714dea19c5a82
ashleydowns <netzhammer15@gmail.com>
2020-02-14 19:02:08 -0600

    OOPS, LAST MINUTE

--- queries.sql
+++ queries.sql
@@ -4,10 +4,16 @@
     employer VARCHAR(20)
     name VARCHAR(20)
     skills VARCHAR(200)
+
 ## Part 2: Test it with SQL
-    SELECT employer
-    FROM techjobs
-    WHERE (location = "Saint Louis")
+    DROP TABLE job;
+
 ## Part 3: Test it with SQL
     DELETE FROM techjobs WHERE (job_id !== null)
+
 ## Part 4: Test it with SQL
+    SELECT name, description
+    FROM skills
+    INNER JOIN job_skills on skills.id = job_skills.skills_id
+    WHERE job_id is not null
+    order by skill.name asc;
